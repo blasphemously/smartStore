f2635f1a79538336c78fd84f9c45ae8218e13190
kujifanm <2041889849@qq.com>
2020-12-05 08:47:28 +0800

    优化

--- src/views/smartStore/index.vue
+++ src/views/smartStore/index.vue
@@ -1,116 +1,114 @@
 <template>
-  <div id="container"></div>
+	<div id="container"></div>
 </template>
 
 <script>
-import * as THREE from 'three'
+	import * as THREE from 'three'
 
-const OrbitControls = require('three-orbit-controls')(THREE)
-import createWall from './wall'
+	const OrbitControls = require('three-orbit-controls')(THREE)
+	import createWall from './wall'
 
-export default {
-  name: "index",
-  data() {
-    return {
-      camera: null,
-      scene: null,
-      renderer: null
-    }
-  },
-  mounted() {
-    this.init()
-  },
-  methods: {
-    init() {
-      this.creatScene()
-      this.creatFloor()
-      this.creatWall()
-      this.creatPoint()
-      this.creatCamera()
-      this.creatRenderer()
-      this.createControls()
-      this.render()
+	export default {
+		name: "index",
+		data() {
+			return {
+				camera: null,
+				scene: null,
+				renderer: null
+			}
+		},
+		mounted() {
+			this.init()
+		},
+		methods: {
+			init() {
+				this.creatScene()
+				this.creatFloor()
+				this.creatWall()
+				this.creatPoint()
+				this.creatCamera()
+				this.creatRenderer()
+				this.createControls()
+				this.render()
 
-    },
-    creatScene() {
-      this.scene = new THREE.Scene()
-    },
-    creatCamera() {
-      this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-      this.camera.position.set(0, 1600, 1000)
-      this.scene.add(this.camera)
-    },
-    creatFloor() {
-      const Texturing = require('../../../public/smartStore/images/floor.jpg')
-      const loader = new THREE.TextureLoader()
-      loader.load(Texturing, (texture) => {
-        texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-        texture.repeat.set(10, 10);
-        const floorGeometry = new THREE.BoxGeometry(2600, 1400, 1)
-        const floorMaterial = new THREE.MeshBasicMaterial({
-          map: texture,
-        });
-        const floor = new THREE.Mesh(floorGeometry, floorMaterial)
-        floor.rotation.x = -Math.PI / 2
-        floor.name = "地面";
-        this.scene.add(floor);
-      })
-    },
-    creatWall() {
-      const matArrayB = new THREE.MeshLambertMaterial({color: '#AFC0CA'})
-      const wallWest = createWall(10, 200, 1400, 0, matArrayB, -1295, 100, 0, "墙面")
-      const wallSouth = createWall(10, 200, 1400, 1, matArrayB, 1295, 100, 0, "墙面")
-      const wallEast = createWall(10, 200, 2600, 1.5, matArrayB, 0, 100, -700, "墙面")
-      const wallNorth = createWall(10, 200, 2600, 1.5, matArrayB, 0, 100, 700, "墙面")
-      this.scene.add(wallWest)
-      this.scene.add(wallSouth)
-      this.scene.add(wallEast)
-      this.scene.add(wallNorth)
-    },
-    creatPoint() {
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3) //模拟远处类似太阳的光源
-      directionalLight.color.setHSL(0.1, 1, 0.95)
-      directionalLight.position.set(0, 200, 0).normalize()
-      this.scene.add(directionalLight)
+			},
+			creatScene() {
+				this.scene = new THREE.Scene()
+			},
+			creatCamera() {
+				this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+				this.camera.position.set(0, 1600, 1000)
+				this.scene.add(this.camera)
+			},
+			creatFloor() {
+				const Texturing = require('../../../public/smartStore/images/floor.jpg') // 只有用require方法,代替load里直接放进的url
+				const loader = new THREE.TextureLoader()
+				loader.load(Texturing, (texture) => {
+					texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+					texture.repeat.set(10, 10);
+					const floorGeometry = new THREE.BoxGeometry(2600, 1400, 1)
+					const floorMaterial = new THREE.MeshBasicMaterial({ //地板使用基础网格材料可以将贴图放上使用MeshLambertMaterial不行
+						map: texture,
+					});
+					const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+					floor.rotation.x = -Math.PI / 2
+					floor.name = "地面";
+					this.scene.add(floor);
+				})
+			},
+			creatWall() {
+				const matArrayB = new THREE.MeshLambertMaterial({
+					color: '#AFC0CA'
+				}) //使用基础网格材料没有反光,无法体现光源
+				createWall(10, 200, 1400, 0, matArrayB, -1295, 100, 0, "墙面", this.scene)
+				createWall(10, 200, 1400, 1, matArrayB, 1295, 100, 0, "墙面", this.scene)
+				createWall(10, 200, 2600, 1.5, matArrayB, 0, 100, -700, "墙面", this.scene)
+				createWall(10, 200, 2600, 1.5, matArrayB, 0, 100, 700, "墙面", this.scene)
+			},
+			creatPoint() {
+				const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3) //模拟远处类似太阳的光源
+				directionalLight.color.setHSL(0.1, 1, 0.95)
+				directionalLight.position.set(0, 200, 0).normalize()
+				this.scene.add(directionalLight)
 
-      const ambient = new THREE.AmbientLight(0xffffff, 1) //AmbientLight,影响整个场景的光源
-      ambient.position.set(0, 0, 0)
-      this.scene.add(ambient)
-      const pointLight = new THREE.PointLight(0xffffff)
-      pointLight.position.set(400, 200, 200)
-      this.scene.add(pointLight)
-    },
-    creatRenderer() {
-      const element = document.getElementById('container')
+				const ambient = new THREE.AmbientLight(0xffffff, 1) //AmbientLight,影响整个场景的光源
+				ambient.position.set(0, 0, 0)
+				this.scene.add(ambient)
+				const pointLight = new THREE.PointLight(0xffffff) //点光源, 使立方体出现棱角
+				pointLight.position.set(400, 200, 200)
+				this.scene.add(pointLight)
+			},
+			creatRenderer() {
+				const element = document.getElementById('container')
 
-      this.renderer = new THREE.WebGLRenderer({
-        antialias: true,//是否开启反锯齿，设置为true开启反锯齿。
-        alpha: true,//是否可以设置背景色透明。
-        // logarithmicDepthBuffer: true//模型的重叠部位便不停的闪烁起来。这便是Z-Fighting问题，为解决这个问题，我们可以采用该种方法
-      })
-      this.renderer.setSize(window.innerWidth, window.innerHeight)
-      this.renderer.setClearColor('#39609B', 1.0)
-      this.renderer.setPixelRatio(window.devicePixelRatio)// 设置分辨率与电脑的分辨率相同
-      element.appendChild(this.renderer.domElement)
-    },
-    render() {
-      this.renderer.render(this.scene, this.camera)
-      requestAnimationFrame(this.render)
-    },
-    createControls() {
-      const controls = new OrbitControls(this.camera, this.renderer.domElement)
-      controls.enableDamping = true;
-      controls.dampingFactor = 0.5;
-      // 视角最小距离
-      controls.minDistance = 100;
-      // 视角最远距离
-      controls.maxDistance = 5000;
-      // 最大角度
-      controls.maxPolarAngle = Math.PI / 2.2;
-      controls.target = new THREE.Vector3(50,50,0);
-    }
-  }
-}
+				this.renderer = new THREE.WebGLRenderer({
+					antialias: true, //是否开启反锯齿，设置为true开启反锯齿。
+					alpha: true, //是否可以设置背景色透明。
+					// logarithmicDepthBuffer: true//模型的重叠部位便不停的闪烁起来。这便是Z-Fighting问题，为解决这个问题，我们可以采用该种方法
+				})
+				this.renderer.setSize(window.innerWidth, window.innerHeight)
+				this.renderer.setClearColor('#39609B', 1.0)
+				this.renderer.setPixelRatio(window.devicePixelRatio) // 设置分辨率与电脑的分辨率相同
+				element.appendChild(this.renderer.domElement)
+			},
+			render() {
+				this.renderer.render(this.scene, this.camera)
+				requestAnimationFrame(this.render)
+			},
+			createControls() {
+				const controls = new OrbitControls(this.camera, this.renderer.domElement)
+				controls.enableDamping = true;
+				controls.dampingFactor = 0.5;
+				// 视角最小距离
+				controls.minDistance = 100;
+				// 视角最远距离
+				controls.maxDistance = 5000;
+				// 最大角度
+				controls.maxPolarAngle = Math.PI / 2.2;
+				controls.target = new THREE.Vector3(50, 50, 0);
+			}
+		}
+	}
 </script>
 
 <style scoped>
--- src/views/smartStore/wall.js
+++ src/views/smartStore/wall.js
@@ -1,13 +1,14 @@
 import * as THREE from "three"
-function createWall(width, height, depth, angle, material, x, y, z, name) {
-    const wallGeometry = new THREE.BoxGeometry(width, height, depth)
-    const wall = new THREE.Mesh(wallGeometry, material)
-    wall.position.x = x
-    wall.position.y = y
-    wall.position.z = z
-    wall.name = name
-    wall.rotation.y += angle * Math.PI; //-逆时针旋转,+顺时针 控制墙体旋转
-    return wall
+
+function createWall(width, height, depth, angle, material, x, y, z, name, scene) {
+	const wallGeometry = new THREE.BoxGeometry(width, height, depth)
+	const wall = new THREE.Mesh(wallGeometry, material)
+	wall.position.x = x
+	wall.position.y = y
+	wall.position.z = z
+	wall.name = name
+	wall.rotation.y += angle * Math.PI; //-逆时针旋转,+顺时针 控制墙体旋转
+	scene.add(wall)
 }
 
 export default createWall
