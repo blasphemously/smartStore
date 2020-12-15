98e2eab59b1166907dbfd372e9b9ee0f3f96de1b
kujifanm <2041889849@qq.com>
2020-12-15 16:08:18 +0800

    修改由于clone共用同一个material导致点击变色事件时所有模型全部变色

--- src/views/smartStore/chargo.js
+++ src/views/smartStore/chargo.js
@@ -13,6 +13,7 @@ function creatCargo(scene,area_x,area_z) {
     // const  box = goods.clone()
 
     const good1 = goodsMash.clone()
+    good1.material=new THREE.MeshLambertMaterial( {color: "#ddffdd"}); //重新添加material,从而使每一个都有新的material,就不会导致一个改变全部改变
     good1.position.set(area_x+50+15,20+15,-(area_z+50+15))
     // good1.position.set(area_x+65+(45*row),45*(boxHeight+1),-(area_z+65+(45*line)))
     good1.name = '货物'
